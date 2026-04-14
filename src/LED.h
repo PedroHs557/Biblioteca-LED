40ff93ab460eca5755f4b71e54ebe34e73aae959
PEDRO HENRIQUE DA SILVA <55506799810@info.local>
2026-04-14 09:28:47 -0300

    commit com comentários

--- src/LED.h
+++ src/LED.h
@@ -27,19 +27,55 @@ private:
     bool estadoPiscar;
     uint16_t repeticoes;
 
+    /**
+     * @brief processa a lógica de piscar o led.
+     */
     void funcaoPiscar();
+
+    /**
+     * @brief processa o desligamento temporarizado do led.
+     */
     void funcaoDesligamento();
 
 public:
-    Led(uint8_t pino); // metodo construtor, deverá
+/**
+ * @brief constrói um objeto led
+ * @param pino o número do pino onde o led está conectado.
+ */
+    Led(uint8_t pino); 
 
+    /**
+     * @brief liga o led continuamente.
+     */
     void ligar();
+
+    /**
+     * @brief liga o led por um tempo determinado
+     * @param tempoLigado_ms tempo, em milissegundos, que o LED ficará ligado.
+     */
     void ligar(uint32_t tempoLigado_ms);
 
+    /**
+     * @brief desliga o led. e cancela os modos automáticos.
+     */
     void desligar();
 
+    /**
+     * @brief inicia o modo de piscar continuamente, com frequência de 1Hz.
+     */
     void piscar();
+
+    /**
+     * @brief inicia o modo de piscar continuamente, com frequência definida.
+     * @param frequencia frequência de piscar, em Hz.
+     */
     void piscar(float frequencia);
+
+    /**
+     * @brief inicia o modo de piscar por quantidade definida, com frequência definida.
+     * @param frequencia frequência de piscar, em Hz.
+     * @param repeticoes quantidade de vezes que o LED irá acender (ex: 3 acende 3 vezes e apaga 3 vezes).
+     */
     void piscar(float frequencia, uint16_t repeticoes);
 
     bool getEstado();
@@ -47,8 +83,15 @@ public:
 
     void setEstado(bool estado);
 
+    /**
+     * @brief inverte o estado do LED (se estiver ligado, desliga; se estiver desligado, liga).
+     * E cancela os modos automáticos.
+     */
     void alterar();
 
+    /**
+     * @brief atualiza o estado do led.
+     */
     void update();
 };
 
